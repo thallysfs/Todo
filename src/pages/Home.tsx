0b631ee9577a2b51a8cf3d2ad3a4125358c5ed9b
thallysfs <thallys@hotmail.com>
2021-11-10 16:45:29 -0300

    Resolução do desafio: conceitos react

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,33 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, data])
+
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({...task}));
+    const taskToUpdate = updatedTasks.find(task => task.id === id);
+
+    if (!taskToUpdate)
+      return;
+    
+    taskToUpdate.done = !taskToUpdate.done;
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
